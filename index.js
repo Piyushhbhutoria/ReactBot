62ba7e8ad1df1f9d9b60989fd2afd96954214938
Piyushhbhutoria <piyush.bhutoria98@gmail.com>
2020-08-06 01:50:58 +0530

    react update

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(function(req, res, next) {
 
 require('./routes/dialogFlowRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT);
